1baed637172c724b8819e252f2d420cfb789797d
Joel <joelmartinez@veltium.com>
2021-11-02 08:15:11 +0000

    Main control task created

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,8 @@ SemaphoreHandle_t xSerialSemaphore;
 //Declarar los objetos de los sensores
 SensorU_t OjosDelante, OjosDetras;
 
-// Tareas
-void TaskOjosDelante( void *pvParameters );
-void TaskOjosDetras( void *pvParameters );
+// Prototipos de funciones
+void ControlTask (void *pvParameters);
 
 void setup() {
 
@@ -16,8 +15,8 @@ void setup() {
   
   OjosDelante.direccionEcho = 1;
   OjosDelante.direccionTrig = 2;
-  OjosDetras.direccionEcho = 3;
-  OjosDetras.direccionTrig = 4;
+  OjosDetras.direccionEcho  = 3;
+  OjosDetras.direccionTrig  = 4;
 
   //Arrancar el semaforo
   if ( xSerialSemaphore == NULL ){
@@ -29,18 +28,34 @@ void setup() {
   // Crear las tareas de lectura de sensores
   xTaskCreate(TaskUltrasonicRead,"OjosDelante",128,(void*)&OjosDelante,2,NULL); 
   xTaskCreate(TaskUltrasonicRead,"OjosDetras",128,(void*)&OjosDetras,3,NULL); 
+
+  //Crear la tarea de control principal
+  xTaskCreate(ControlTask,"Control",256,(void*)&ControlTask,3,NULL); 
 }
 
 void loop(){
-  Serial.println("Ojos Delante");
-  Serial.println(OjosDelante.distancia);
-  Serial.println("Ojos Detras");
-  Serial.println(OjosDetras.distancia);
-  delay(1000);
 }
 
 /*--------------------------------------------------*/
 /*--------------------- Tareas ---------------------*/
 /*--------------------------------------------------*/
+
+/****************************************************************
+ * Función ControlTask
+ * Funcion pensada para ejecutarse como tarea.
+ * Tarea de control principal de Moevious 
+ * params: Ninguno
+ ****************************************************************/
+void ControlTask (void *pvParameters){
+
+  for(;;){
+    Serial.println("Ojos Delante");
+    Serial.println(OjosDelante.distancia);
+    Serial.println("Ojos Detras");
+    Serial.println(OjosDetras.distancia);
+    
+    duerme(100);
+  }
+}
 
 
