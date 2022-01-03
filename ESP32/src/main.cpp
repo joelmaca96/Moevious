0f0b747131d197986b8af63c0a587ad8b905f161
joel martinez <joelmartinez@veltium.com>
2022-01-03 09:37:21 +0100

    Rrformat  and create ESP32 project

---
+++ ESP32/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Estoy vivo!!!!\n");
+  printf("Y puedo usar printf!!\n");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
